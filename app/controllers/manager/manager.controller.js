b10809c84c85a5efdbeab3c221669b0f90e61500
mehrdadsadeghi228 <63063456+mehrdadsadeghi228@users.noreply.github.com>
2023-10-17 13:02:33 -0700

    wroking on controller
    manager for adding and manage doctor list & overview

---
+++ app/controllers/manager/manager.controller.js
@@ -0,0 +1,108 @@
+require('dotenv').config();
+const Controller = require('../base.Controller');
+
+const {StatusCodes: HttpStatus} = require("http-status-codes");
+const createHttpError = require('http-errors');
+const { codeERSali } = require('../../utils/utils');
+
+
+class managerControllerClass extends Controller{
+
+    async AddDoctor(req,res,next){
+        try {
+            const {userFullName,resume,dayOfWeekFree,mobile} =req.body;
+
+            const query=await dockerModel.create({userFullName:userFullName,resume:resume,dayOfWeekFree:dayOfWeekFree,mobile:mobile} );
+            if(!query) createHttpError.InternalServerError("there was problems for adding Docker ");
+            return res.status(HttpStatus.OK).json({
+                StatusCode:HttpStatus.OK,
+                message:'Adding it was success the id  '+query._id
+            });
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    async SendsMobileCodeAdmin(req, res, next) {
+        try {
+            var location = '/AdminManager/SendsCodeAdmin';
+            const code = codeERSali();
+            const { userName } = req.Admin;
+            const resultSearching = await dockerModel.findOne({ userFullName: userName });
+            if (!resultSearching) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    statusCodes: HttpStatus.BAD_REQUEST,
+                    message: "user name is not exist "
+                });
+            }
+            if (!resultSearching.Active) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    statusCodes: HttpStatus.BAD_REQUEST,
+                    message: " user name is delete before "
+                });
+            }
+
+            const updateModel = dockerModel.findOneAndUpdate({ userFullName: userName }, {
+                '$set': { 'otpMobile.code': code }
+            }).exec();
+            res.status(HttpStatus.BAD_REQUEST).json({
+                statusCodes: HttpStatus.BAD_REQUEST,
+                message: `the code is ${code} for verify mobile id & and user name is ` + updateModel._id + updateModel.userName
+            });
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getVerifyMobileAdmin(req, res, next) {
+        try {
+            const { username } = req.Admin;
+            const { userCode } = req.body;
+            const resultSearching = await dockerModel.findOne({ userFullName: username });
+            if (!resultSearching) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    statusCodes: HttpStatus.BAD_REQUEST,
+                    message: "user name is not exist "
+                });
+            }
+            if (!resultSearching.Active) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    statusCodes: HttpStatus.BAD_REQUEST,
+                    message: "user name is delete before "
+                });
+            }
+            if (resultSearching.isMobile) {
+                return res.status(HttpStatus.OK).json({
+                    statusCodes: HttpStatus.OK,
+                    message: " email is verify before"
+                });
+            }
+            if (!userCode) createHttpError.NotImplemented('code receive can Not br empty ');
+
+            if (resultSearching.otpMobile.code == userCode) {
+                await dockerModel.findOneAndUpdate({ userFullName: username }, {
+                    isMobile: true
+                });
+                return res.status(HttpStatus.OK).json({
+                    statusCodes: HttpStatus.OK,
+                    message: "verify  email is sucess "
+                });
+            };
+
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                statusCodes: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: " there was a problem the code is not right or server in issus"
+            });
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+}
+
+module.exports={
+    managerControllerClass:new managerControllerClass()
+}
--- app/controllers/overview.controller.js
+++ app/controllers/overview.controller.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 const Controller = require('../base.Controller');
 
 const {StatusCodes: HttpStatus, StatusCodes} = require("http-status-codes");
-const {DockerModel} = require('../model/doctor.model');
 const createHttpError = require('http-errors');
-
+const {dockerModel}=require('../model/doctor.model');
 
 class OverviewControllerClass extends Controller{
 
     async getListOfDoctor(req,res,next){
         try {
-            const query=await DockerModel.find({},{userFullName:1});
+            const query=await dockerModel.find({},{userFullName:1});
             if(!query) createHttpError.NotFound("there is any list of dockers ");
             return res.status(StatusCodes.OK).json({
                 StatusCode:StatusCodes.OK,
@@ -23,7 +22,7 @@ class OverviewControllerClass extends Controller{
 
     async getListOfDoctorWithTime(req,res,next){
         try {
-            const query=await DockerModel.find({},{userFullName:1,dayOfWeekFree:1,resume:1});
+            const query=await dockerModel.find({},{userFullName:1,dayOfWeekFree:1,resume:1});
             if(!query) createHttpError.NotFound("there is any list of dockers ");
             return res.status(StatusCodes.OK).json({
                 StatusCode:StatusCodes.OK,
@@ -36,6 +35,21 @@ class OverviewControllerClass extends Controller{
     }
 
 
+    async getResumeDoctor(req,res,next){
+        try {
+            const {username}=req.body;
+
+            const query=await dockerModel.find({userFullName:username},{userFullName:1,resume:1});
+            if(!query) createHttpError.NotFound("there is any list of dockers ");
+            return res.status(StatusCodes.OK).json({
+                StatusCode:StatusCodes.OK,
+                message:'Docker And Resume '+query
+              
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports={
